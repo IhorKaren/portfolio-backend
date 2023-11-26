598ae061e88a281c996e982007dabe21e2edd7e9
Ihor Karen <karenihor@gmail.com>
2023-11-26 11:19:39 +0200

    reverse response array

--- controllers/projects/getAllProjects.js
+++ controllers/projects/getAllProjects.js
@@ -3,6 +3,10 @@ const { Project } = require("../../models/project/project");
 const getAllProjects = async (req, res) => {
   const result = await Project.find({}, "-createdAt -updatedAt");
 
+  if (result) {
+    result.reverse();
+  }
+
   res.status(200).json(result);
 };
 
